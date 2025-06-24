5a7496e5dedeb70d434ba9da0befd12335f2f267
Akash <akash.workloadmaster@gmail.com>
2025-06-24 13:21:52 +0530

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
     setLoading(true);
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/send/mail",
+        "https://responsive-gym-website-backend.onrender.com/send/mail",
         {
           name,
           email,
